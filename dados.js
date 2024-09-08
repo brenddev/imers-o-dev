18b2712cab0e998f832dfa3856b0c4df5df124de
brenddev <brenda07lande@gmail.com>
2024-09-07 21:35:52 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Proposta de Negócio",
+        descricao: " A história gira em torno de Shin Ha-ri, uma jovem funcionária que, para ajudar uma amiga, aceita ir a um encontro às cegas em seu lugar. No entanto, o encontro é com o CEO da empresa onde ela trabalha, Kang Tae-mu, um homem rico e poderoso, conhecido por ser exigente e frio.",
+        link: "https://www.netflix.com/br/",
+        
+    },
+    {
+        titulo: "Rainha das Lágrimas",
+        descricao: "é um dorama que acompanha a história de Hae-in, uma herdeira de um grande império empresarial, e Hyun-woo, um diretor jurídico. O casal, que se apaixonou e se casou ainda jovens, enfrenta uma crise em seu relacionamento que os leva à beira do divórcio.",
+        link: "https://www.netflix.com/br/",
+        
+    },
+    {
+        titulo: "Mr. Queen",
+        descricao: " é uma comédia histórica que mistura elementos de fantasia e romance. A trama gira em torno de Jang Bong-hwan, um chef de cozinha da Coreia do Sul que, de forma inesperada, acaba preso no corpo da rainha Kim So-yong durante a dinastia Joseon.",
+        link: "https://www.netflix.com/br/",
+        
+    }
+];
